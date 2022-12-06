7d01b6770c9545ec4bbf50c742eb3c1aa0402f9c
Job Karani <jobkarani084j@gmail.com>
2022-12-06 02:41:27 -0800

    dbport

--- gallery/urls.py
+++ gallery/urls.py
@@ -18,6 +18,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('lib.urls')),
