abad297c693de9ff54e98226bccd3f58c9d58868
Job Karani <jobkarani084j@gmail.com>
2022-12-06 03:12:29 -0800

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -96,11 +96,11 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':config('DB_NAME'),
-        'USER':config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'yIt3g47P6aWEiaXOqtXo',
+        'HOST':'containers-us-west-127.railway.app',
+        'PORT':'7935',
     }
 }
 
